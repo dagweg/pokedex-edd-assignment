aa6cd435d1a9d9ba05619b7921899d52172446e3
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-10 00:54:11 +0300

    feat(fix): bring options configuratoin to the top; register the PokemonContext to the DI container

--- Program.cs
+++ Program.cs
@@ -1,33 +1,56 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using pokedex.Middlewares;
+using pokedex.Models;
+using pokedex.Options;
 using pokedex.Services;
+using pokedex.Services.MongoDB;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+{
+    BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
 
-// register PokemonService as a service
-builder.Services.AddScoped<IPokemonService, PokemonService>();
+    // Load mongo options from appsettings.json
+    // We'll use options pattern to configure the MongoDB Context inside the service
+    builder.Services.Configure<MongoDbOptions>(
+        builder.Configuration.GetSection(MongoDbOptions.SectionName)
+    );
+
+    // Register MongoService for the Pokemon collection
+    builder.Services.AddSingleton<IPokemonContext, PokemonContext>();
+
+    builder.Services.AddScoped<IPokemonService, PokemonService>();
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
-// register middlewares
-app.UseMiddleware<ErrorHandlingMiddleware>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // register middlewares
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    // app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-// app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
