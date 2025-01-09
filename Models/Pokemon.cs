447bbaf2e5e00108b74aed57787a23edd9396cd3
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-10 00:49:05 +0300

    bson representation changed to string

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace pokedex.Models
 {
     public class Pokemon
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public string? Id { get; set; }
         public string? Name { get; set; }
         public string? Type { get; set; }
