d3d2c349567239d4547c919e4074c17dd92e3c8c
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-09 17:36:36 +0300

    feat: register services to di container, use middlewares and map controllers

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using pokedex.Middlewares;
+using pokedex.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// register PokemonService as a service
+builder.Services.AddScoped<IPokemonService, PokemonService>();
+
+var app = builder.Build();
+
+// register middlewares
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
