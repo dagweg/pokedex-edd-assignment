8a061dbeca37bd81af74c1632dc0c451ee33031f
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-09 16:28:06 +0300

    feat: pokemon service interface

---
+++ Services/IPokemonService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using pokedex.Models;
+
+namespace pokedex.Services
+{
+    public interface IPokemonService
+    {
+        Task<List<Pokemon>> GetPokemonsAsync();
+        Task<Pokemon> GetPokemonByIdAsync(string id);
+        Task<Pokemon> GetPokemonByNameAsync(string name);
+        Task<Pokemon> AddPokemonAsync(Pokemon newPokemon);
+        Task<Pokemon> UpdatePokemonAsync(string id, Pokemon updatedPokemon);
+        Task<bool> DeletePokemonAsync(string id);
+    }
+}
