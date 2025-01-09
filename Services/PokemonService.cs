5f18f5c176d10fe99bfd309d819cb2a9a1cdb9b8
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-09 17:37:00 +0300

    feat: partial updates for pokemon

--- Services/PokemonService.cs
+++ Services/PokemonService.cs
@@ -47,38 +47,38 @@ namespace pokedex.Services
 
         public async Task<List<Pokemon>> GetPokemonsAsync()
         {
-            await Task.CompletedTask; // delete when using actual db call
+            await Task.CompletedTask; // delete when using async db call
 
             return pokemons;
         }
 
         public async Task<Pokemon> GetPokemonByIdAsync(string id)
         {
-            await Task.CompletedTask; // delete when using actual db call
+            await Task.CompletedTask; // delete when using async db call
 
             var pokemon = pokemons.FirstOrDefault(pokemon => pokemon.Id == id);
             if (pokemon == null)
             {
-                throw new Exception("Pokemon not found");
+                throw new KeyNotFoundException("Pokemon not found");
             }
             return pokemon;
         }
 
         public async Task<Pokemon> GetPokemonByNameAsync(string name)
         {
-            await Task.CompletedTask; // delete when using actual db call
+            await Task.CompletedTask; // delete when using async db call
 
             var pokemon = pokemons.FirstOrDefault(pokemon => pokemon.Name == name);
             if (pokemon == null)
             {
-                throw new Exception("Pokemon not found");
+                throw new KeyNotFoundException("Pokemon not found");
             }
             return pokemon;
         }
 
         public async Task<Pokemon> AddPokemonAsync(Pokemon newPokemon)
         {
-            await Task.CompletedTask; // delete when using actual db call
+            await Task.CompletedTask; // delete when using async db call
 
             Guid id = Guid.NewGuid();
             newPokemon.Id = id.ToString();
@@ -90,25 +90,30 @@ namespace pokedex.Services
 
         public async Task<Pokemon> UpdatePokemonAsync(string id, Pokemon updatedPokemon)
         {
-            await Task.CompletedTask; // delete when using actual db call
+            await Task.CompletedTask; // delete when using async db call
 
             var index = pokemons.FindIndex(pokemon => pokemon.Id == id);
             if (index == -1)
             {
-                throw new Exception("Pokemon not found");
+                throw new KeyNotFoundException("Pokemon not found");
             }
-            pokemons[index] = updatedPokemon;
-            return updatedPokemon;
+            pokemons[index].Id = updatedPokemon.Id ?? pokemons[index].Id;
+            pokemons[index].Name = updatedPokemon.Name ?? pokemons[index].Name;
+            pokemons[index].Type = updatedPokemon.Type ?? pokemons[index].Type;
+            pokemons[index].Ability = updatedPokemon.Ability ?? pokemons[index].Ability;
+            pokemons[index].Level = updatedPokemon.Level ?? pokemons[index].Level;
+
+            return pokemons[index];
         }
 
         public async Task<bool> DeletePokemonAsync(string id)
         {
-            await Task.CompletedTask; // delete when using actual db call
+            await Task.CompletedTask; // delete when using async db call
 
             var pokemon = pokemons.FirstOrDefault(pokemon => pokemon.Id == id);
             if (pokemon == null)
             {
-                throw new Exception("Pokemon not found");
+                throw new KeyNotFoundException("Pokemon not found");
             }
             pokemons.Remove(pokemon);
             return true;
